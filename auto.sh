38a34203383ffab831e6f99c71ce9fffb3a920b7
AikoCute <aikocute@icloud.com>
2022-10-26 16:05:27 +0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -2,3 +2,4 @@
 
 sudo git clone -b dev https://github.com/AikoCute-Offical/Paralells-Desktop.git
 sudo chmod +x Paralells-Desktop/install.sh && Paralells-Desktop/install.sh
+sudo rm -rf Paralells-Desktop
