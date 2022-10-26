c06d40f62455c2bf33cd147330dd45b144141371
AikoCute <aikocute@icloud.com>
2022-10-26 16:11:54 +0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #cấp quyền Full disk cho terminal
-sudo chmod 777 Applications/Utilities/Terminal.app/Contents/MacOS/Terminal
+sudo chmod 777 /System/Applications/Utilities/Terminal.app/Contents/MacOS/Terminal
 
 sudo git clone -b dev https://github.com/AikoCute-Offical/Paralells-Desktop.git
 sudo chmod +x Paralells-Desktop/install.sh && Paralells-Desktop/install.sh
